76f10a3590565660ffde48e75c70fa8a39095c7d
Lucas Shen <lshens@hotmail.com>
2017-10-22 00:26:58 -0200

    Update ignore

--- backend/core/src/main/java/com/hack/debit/jpa/JPAEntity.java
+++ backend/core/src/main/java/com/hack/debit/jpa/JPAEntity.java
@@ -1,17 +1,27 @@
 package com.hack.debit.jpa;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
 /**
  * Created by arnaldo on 21/10/2017.
  */
-
 @MappedSuperclass
-public class JPAEntity {
+public class JPAEntity implements Serializable {
 
-    @Id
+	private static final long serialVersionUID = -5446172037933111047L;
+	
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
